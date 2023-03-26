922747cefe6b79e6d373101602bf53bec760d5c8
andrewtan2000 <andrew_tan2000@hotmail.com>
2023-03-26 21:38:01 +0800

    Sending Postman Get Request

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -1,11 +1,11 @@
 package com.example.demo.dao;
 
-import  com.example.demo.model.Person;
+import com.example.demo.model.Person;
 import org.springframework.stereotype.Repository;
 
-import  java.util.ArrayList;
-import  java.util.List;
-import  java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
@@ -14,7 +14,12 @@ public class FakePersonDataAccessService implements PersonDao {
 
     @Override
     public int insertPerson(UUID id, Person person) {
-        DB.add(new Person(id,person.getName()));
+        DB.add(new Person(id, person.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
     }
 }
